8ef461258eea29b55f1ad5383a3a839310513dda
Lukas Friman <lukas@dreamplay.net>
2025-06-23 11:46:14 +0200

    Removed old devtest.

--- xs_raw/tests/schema/mod.rs
+++ xs_raw/tests/schema/mod.rs
@@ -1,6 +1,6 @@
 use xmlity::types::utils::XmlRoot;
 // use xsd::schema as xs;
-use xs_raw::{xs, xs_custom};
+use xs_raw::xs;
 
 pub mod top_level_attribute_groups;
 pub mod top_level_complex_type;
@@ -13,41 +13,4 @@ fn schema_deserialize() {
     let xml = include_str!("../../../schemas/XMLSchema.xsd");
 
     let _schema: XmlRoot<xs::Schema> = xmlity_quick_xml::from_str(xml).unwrap();
-}
-
-#[test]
-fn devtest() {
-    let xml = r###"<xs:annotation xmlns:xs="http://www.w3.org/2001/XMLSchema">
-          <xs:documentation>
-   This branch is short for
-   &lt;complexContent>
-   &lt;restriction base="xs:anyType">
-   ...
-   &lt;/restriction>
-   &lt;/complexContent></xs:documentation>
-        </xs:annotation>"###;
-
-    let seq: xs::Annotation = xmlity_quick_xml::from_str(xml).unwrap();
-
-    println!("{:?}", seq);
-
-    let xml = r###"<xs:sequence xmlns:xs="http://www.w3.org/2001/XMLSchema" id="TestId">
-        <xs:annotation>
-          <xs:documentation>
-   This branch is short for
-   &lt;complexContent>
-   &lt;restriction base="xs:anyType">
-   ...
-   &lt;/restriction>
-   &lt;/complexContent></xs:documentation>
-        </xs:annotation>
-        <xs:element ref="xs:openContent" minOccurs="0"/>
-        <xs:group ref="xs:typeDefParticle" minOccurs="0"/>
-        <xs:group ref="xs:attrDecls"/>
-        <xs:group ref="xs:assertions"/>
-      </xs:sequence>"###;
-
-    let seq: xs::Sequence = xmlity_quick_xml::from_str(xml).unwrap();
-
-    println!("{:?}", seq);
 }
